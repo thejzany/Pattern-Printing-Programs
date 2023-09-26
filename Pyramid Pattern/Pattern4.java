30aba6bd6f6175ac7850d27eee63ba2301bd5b27
thejzany <jaysohani10@gmail.com>
2023-09-26 23:52:09 +0530

    Pyramid Shaped Pattern Printing Programs

---
+++ Pyramid Pattern/Pattern4.java
@@ -0,0 +1,24 @@
+Pattern4:
+    *
+   ***
+  *****
+ *******
+*********
+
+package com.jsp.pyramidpattern;
+
+public class Pattern4 {
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int s = 1; s <= n - i; s++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= 2 * i - 1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
